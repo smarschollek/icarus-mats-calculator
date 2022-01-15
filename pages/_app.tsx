d629fb1708907b8bb7e2a510ead1c13027aa0d6f
Stephan Marschollek <stephan.marschollek@gmail.com>
2022-01-15 16:32:40 +0100

    adds bootstrap styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
+  
 }
 
 export default MyApp
