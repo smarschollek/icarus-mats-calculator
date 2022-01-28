1c343f73c5217c3d977c35169e1eee7e56e1b032
Stephan Marschollek <stephan.marschollek@gmail.com>
2022-01-28 23:37:52 +0100

    adds info block when screens are too small

--- src/Main/MainPage.tsx
+++ src/Main/MainPage.tsx
@@ -1,9 +1,17 @@
-import { useState } from 'react'
-import styled from 'styled-components'
-import { Items } from '../data/items'
-import { Item } from '../types/Item'
-import LeftContainer from './components/LeftContianer'
-import RightContainer from './components/RightContainer'
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Items } from '../data/items';
+import { Item } from '../types/Item';
+import LeftContainer from './components/LeftContianer';
+import RightContainer from './components/RightContainer';
+
+const InfoContainer = styled.div`
+	width: 100%;
+	height: 100%;
+	display: grid;
+	place-items: center;
+
+`;
 
 const Container = styled.div`
   max-width: 1400px;
@@ -11,19 +19,35 @@ const Container = styled.div`
   margin-left: auto;
   margin-right: auto;
   display: flex;
-`
+`;
 
-const sortedItems = Items.sort((a,b) => a.name > b.name ? 1 : -1)
+const sortedItems = Items.sort((a,b) => a.name > b.name ? 1 : -1);
+
+
 
 const MainPage = () => {
+	const [width, setWidth] = useState<number | null>(null);
+	const [selectedItem, setSelectedItem] = useState<Item>(sortedItems[0]);
 
-    const [selectedItem, setSelectedItem] = useState<Item>(sortedItems[0])
+	window.addEventListener('resize', () => setWidth(window.innerWidth));
 
-  return (
-    <Container>
-      <LeftContainer items={sortedItems} onItemSelected={item => setSelectedItem(item)}/>
-      <RightContainer selectedItem={selectedItem}/>
-    </Container>
-  )
-}
-export default MainPage
+	useEffect(() => {
+		setWidth(window.innerWidth);
+	}, []);
+
+	if(width && width < 1400) {
+		return (
+			<InfoContainer>
+				The width of the device is to small for this app.
+			</InfoContainer>		
+		);
+	}
+ 	 
+	return (
+		<Container>
+			<LeftContainer items={sortedItems} onItemSelected={item => setSelectedItem(item)}/>
+			<RightContainer selectedItem={selectedItem}/>
+		</Container>
+	);
+};
+export default MainPage;
