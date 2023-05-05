ba2fd7f7dccac88dde8323d2c1c55e4a9f5c4756
Alexander Gabriel <alexander.gabriel@digital-infinity.de>
2023-05-06 01:55:59 +0200

    images images images

--- getimage.php
+++ getimage.php
@@ -3,7 +3,8 @@ $urls = file("getimage.txt");
 foreach($urls as $url) {
     $filename = explode("/", $url);
     $filename = $filename[7];
-    if(!file_exists($filename)) {
+    if(!file_exists("public/images/$filename")) {
+        echo "downloding $filename\n\r";
         file_put_contents("public/images/$filename", fopen("$url", 'r'));
     }
 }
